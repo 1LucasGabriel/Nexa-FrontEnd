ecb317c57f752b711e85f85611393e2139777f37
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2026-03-22 16:17:36 -0300

    desenvolvendo a parte visual do login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    providePrimeNG({ theme: {preset: Aura} })
+    providePrimeNG({ theme: {preset: Aura} }),
+    provideAnimations(),
+    provideHttpClient()
   ]
 };
