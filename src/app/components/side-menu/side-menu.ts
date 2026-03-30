cd9162f5c3c9c0c168720b106cf0e37cbdbdd1f1
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2026-03-29 22:04:17 -0300

    entregando menu lateral 90% pronto

--- src/app/components/side-menu/side-menu.ts
+++ src/app/components/side-menu/side-menu.ts
@@ -1,43 +1,48 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { MenuItem, MessageService, PrimeIcons } from 'primeng/api';
 import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
 
 
 @Component({
   selector: 'app-side-menu',
-  imports: [MenuModule],
+  imports: [MenuModule, AvatarModule, BadgeModule],
   providers: [MessageService],
   templateUrl: './side-menu.html',
   styleUrl: './side-menu.scss',
 })
 export class SideMenu implements OnInit {
- private messageService = inject(MessageService);
+  @Input() userName: string = '';
+  @Input() userRole: string = '';
+
+  // private messageService = inject(MessageService);
   items: MenuItem[] | undefined;
 
   ngOnInit() {
     this.items = [
       {
         label: '',
-        
-      },
-      {
-        label: '',
         items: [
-          { label: 'Dashboards', icon: PrimeIcons.OBJECTS_COLUMN },
-          { label: 'Funcionários', icon: PrimeIcons.USERS },
-          { label: 'Alojamento', icon: PrimeIcons.HOME },
-          { label: 'Veículos', icon: PrimeIcons.CAR },
-          { label: 'Relatórios', icon: PrimeIcons.CLIPBOARD },
-          { label: 'Movimentações', icon: PrimeIcons.REPLY },
-          { label: 'Ajustes', icon: PrimeIcons.COG },
+          { label: 'Dashboards', icon: PrimeIcons.OBJECTS_COLUMN, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Funcionários', icon: PrimeIcons.USERS, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Alojamento', icon: PrimeIcons.HOME, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Veículos', icon: PrimeIcons.CAR, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Relatórios', icon: PrimeIcons.CLIPBOARD, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Movimentações', icon: PrimeIcons.REPLY, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+
+          { label: 'Ajustes', icon: PrimeIcons.COG, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
         ]
       },
-      {
-        label: '',
-        items: [
-          { label: '', icon: PrimeIcons.SIGN_OUT }
-        ]
-      }
     ];
+  }
+
+  public irPara(rota: string) {
+    console.log("FUTURAMENTE IRÁ NAVEGAR PARA:", rota);
   }
 }
