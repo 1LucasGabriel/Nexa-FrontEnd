8077f02e634b7e6c40203341492ba89da94b562b
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2026-04-12 16:32:03 -0300

    entregando menu

--- src/app/components/side-menu/side-menu.ts
+++ src/app/components/side-menu/side-menu.ts
@@ -16,37 +16,31 @@ export class SideMenu implements OnInit {
   @Input() userName: string = '';
   @Input() userRole: string = '';
 
-  // private messageService = inject(MessageService);
   items: MenuItem[] | undefined;
+  isExpanded = false;
 
   ngOnInit() {
     this.items = [
       {
         label: '',
         items: [
-          { label: 'Dashboards', icon: PrimeIcons.OBJECTS_COLUMN, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Funcionários', icon: PrimeIcons.USERS, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Alojamento', icon: PrimeIcons.HOME, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Veículos', icon: PrimeIcons.CAR, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Relatórios', icon: PrimeIcons.CLIPBOARD, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Movimentações', icon: PrimeIcons.REPLY, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
-
-          { label: 'Ajustes', icon: PrimeIcons.COG, iconStyle: { color: 'white' }, command: () => { this.irPara('') } },
+          { label: 'Dashboard', icon: PrimeIcons.OBJECTS_COLUMN, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Funcionários', icon: PrimeIcons.USERS, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Alojamento', icon: PrimeIcons.HOME, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Veículos', icon: PrimeIcons.CAR, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Relatórios', icon: PrimeIcons.CLIPBOARD, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Movimentações', icon: PrimeIcons.REPLY, iconStyle: { color: 'white' }, command: () => this.irPara('') },
+          { label: 'Ajustes', icon: PrimeIcons.COG, iconStyle: { color: 'white' }, command: () => this.irPara('') },
         ]
-      },
+      }
     ];
   }
 
   public irPara(rota: string) {
-    console.log("FUTURAMENTE IRÁ NAVEGAR PARA:", rota);
+    console.log('FUTURAMENTE IRÁ NAVEGAR PARA:', rota);
   }
 
   public sair() {
-    console.log("FUTURAMENTE IRA DESLOGAR O USUÁRIO")
+    console.log('FUTURAMENTE IRA DESLOGAR O USUÁRIO');
   }
 }
